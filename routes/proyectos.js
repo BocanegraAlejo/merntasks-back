be1ef3047581e3de66bf2072dd87bac827ef6fc8
4l3003 <44855372+4l3003@users.noreply.github.com>
2021-01-02 23:36:34 -0300

    first commit

---
+++ routes/proyectos.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
+
+// Crea proyectos
+// api/proyectos
+router.post('/',
+    auth,
+    [
+        check('nombre','El nombre del proyecto es Obligatorio').not().isEmpty()
+    ],
+    proyectoController.crearProyecto
+)
+// Obtener todos los proyectos
+router.get('/',
+    auth,
+    proyectoController.obtenerProyectos
+)
+// actualizar proyecto via ID
+router.put('/:id',
+    auth,
+    [
+        check('nombre','El nombre del proyecto es Obligatorio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+)
+
+// eliminar un proyecto
+router.delete('/:id',
+    auth,
+    proyectoController.eliminarProyecto
+)
+
+module.exports = router;
